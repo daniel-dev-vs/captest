116f540c1e0f8f90510f4fa1b869544b3260b118
Daniel Ferreira <danenoisturbera@gmail.com>
2018-07-24 22:29:05 -0300

    Adicionando task as assinaturas de métodos da interface

--- CapptaApi/Services/ITransacaoService.cs
+++ CapptaApi/Services/ITransacaoService.cs
@@ -8,12 +8,12 @@ namespace CapptaApi.Services
 {
     interface ITransacaoService
     {
-        IEnumerable<Transacao> ConsultaPorCnpj(string cnpj, int bandeira);
-        IEnumerable<Transacao> ConsultaPorData(DateTime data, int bandeira);
-        IEnumerable<Transacao> ConsultaPorBandeira(string bandeira);
-        IEnumerable<Transacao> ConsultaPorAdquirente(string adquirente);
-        IEnumerable<Transacao> ConsultaPorCnpjDataAtualMastercard(string bandeira);
-        IEnumerable<Transacao> ConsultaPorStoneÙltimos30Dias(string bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorCnpj(string cnpj, int bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorData(DateTime data, int bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorBandeira(string bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorAdquirente(string adquirente);
+        Task<IEnumerable<Transacao>> ConsultaPorCnpjDataAtualMastercard(string bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorStoneÙltimos30Dias(string bandeira);
 
     }
 }
