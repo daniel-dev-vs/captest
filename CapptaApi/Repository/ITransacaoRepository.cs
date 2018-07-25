fe2492b58a433346fcc27ecf24975d36c83a202a
Daniel Ferreira <danenoisturbera@gmail.com>
2018-07-24 22:29:35 -0300

    Adicionando Repositório Transação

---
+++ CapptaApi/Repository/ITransacaoRepository.cs
@@ -0,0 +1,18 @@
+﻿using CapptaApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapptaApi.Repository
+{
+    interface ITransacaoRepository
+    {
+        Task<IEnumerable<Transacao>> ConsultaPorCnpj(string cnpj, int bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorData(DateTime data, int bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorBandeira(string bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorAdquirente(string adquirente);
+        Task<IEnumerable<Transacao>> ConsultaPorCnpjDataAtualMastercard(string bandeira);
+        Task<IEnumerable<Transacao>> ConsultaPorStoneÙltimos30Dias(string bandeira);
+    }
+}
