ef542fb07573718240d9c7071566b83c181cbaa7
Daniel Ferreira <danenoisturbera@gmail.com>
2018-07-24 22:21:14 -0300

    Adicionando TransacaoService

---
+++ CapptaApi/Services/ITransacaoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CapptaApi.Models;
+
+namespace CapptaApi.Services
+{
+    interface ITransacaoService
+    {
+        IEnumerable<Transacao> ConsultaPorCnpj(string cnpj, int bandeira);
+        IEnumerable<Transacao> ConsultaPorData(DateTime data, int bandeira);
+        IEnumerable<Transacao> ConsultaPorBandeira(string bandeira);
+        IEnumerable<Transacao> ConsultaPorAdquirente(string adquirente);
+        IEnumerable<Transacao> ConsultaPorCnpjDataAtualMastercard(string bandeira);
+        IEnumerable<Transacao> ConsultaPorStoneÙltimos30Dias(string bandeira);
+
+    }
+}
